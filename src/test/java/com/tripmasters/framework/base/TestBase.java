87f61cc8e7748f34439cd42d5578907792c215ed
kalpana-QA <kalpana.kaushik@impactqa.com>
2020-03-26 13:20:36 +0530

    wait

--- src/test/java/com/tripmasters/framework/base/TestBase.java
+++ src/test/java/com/tripmasters/framework/base/TestBase.java
@@ -126,8 +126,8 @@ public class TestBase {
 		} finally {
 			if (flag) {
 				driver.manage().deleteAllCookies();
-				driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 				String url = JsonDataReader.getJSONData("URL");
 				driver.get(url);
 				//Logs.info("The given URL '" + url + "' launch successfully for " + platform + " platform and " + browser
